de38f05c19484f2da5dd934fed962e1185260558
litblock <jonathanpeng2017@gmail.com>
2024-07-29 12:31:25 -0400

    folder icons

--- src/main/java/com/thelitblock/texteditor/FolderSetup.java
+++ src/main/java/com/thelitblock/texteditor/FolderSetup.java
@@ -3,7 +3,6 @@ package com.thelitblock.texteditor;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.Stage;
 
 import java.io.File;
 import java.util.Objects;
@@ -11,6 +10,8 @@ import java.util.Objects;
 public class FolderSetup {
     private TreeView<String> folderTreeView;
     private TreeItem<String> rootItem;
+    private static final String FOLDER_ICON = "\uD83D\uDCC1"; // 📁
+    private static final String FILE_ICON = "\uD83D\uDCC4"; // 📄
 
     public FolderSetup(HBox searchBar, TreeView<String> folderTreeView, TreeItem<String> rootItem) {
         this.folderTreeView = folderTreeView;
@@ -20,10 +21,43 @@ public class FolderSetup {
         Button openDirButton = new Button("Open Folder");
         openDirButton.setOnAction(e -> openFolder());
         searchBar.getChildren().add(openDirButton);
+
+        setupTreeViewCellFactory();
     }
 
     public TreeView<String> getFolderTreeView() {
         return folderTreeView;
+    }
+
+    private void setupTreeViewCellFactory() {
+        folderTreeView.setCellFactory(tv -> new TreeCell<String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                    setGraphic(null);
+                }
+                else {
+                    TreeItem<String> treeItem = getTreeItem();
+                    if (treeItem != null && treeItem.getGraphic() != null) {
+                        File file = (File) treeItem.getGraphic().getUserData();
+                        if (file.isDirectory()) {
+                            setText(FOLDER_ICON + " " + item);
+                            setStyle("-fx-text-fill: #c678dd;");
+                        }
+                        else {
+                            setText(FILE_ICON + " " + item);
+                            setStyle("-fx-text-fill: #61afef;");
+                        }
+                    }
+                    else {
+                        setText(item);
+                        setStyle("");
+                    }
+                }
+            }
+        });
     }
 
     public void setupFolderTreeView() {
@@ -43,12 +77,12 @@ public class FolderSetup {
             TreeItem<String> item = new TreeItem<>(file.getName());
             parent.getChildren().add(item);
 
+            Label label = new Label(file.getName());
+            label.setUserData(file);
+            item.setGraphic(label);
+
             if (file.isDirectory()) {
                 createTree(file, item);
-            }
-            else {
-                item.setGraphic(new Label(file.getName()));
-                item.getGraphic().setUserData(file);
             }
         }
     }
@@ -59,7 +93,11 @@ public class FolderSetup {
 
         if (selectedDirectory != null) {
             rootItem.getChildren().clear();
+            Label rootLabel = new Label(selectedDirectory.getName());
+            rootLabel.setUserData(selectedDirectory);
+            rootItem.setGraphic(rootLabel);
             createTree(selectedDirectory, rootItem);
+            folderTreeView.setRoot(rootItem);
         }
     }
 }
